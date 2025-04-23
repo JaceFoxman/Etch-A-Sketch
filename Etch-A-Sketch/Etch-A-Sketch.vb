2ee7ebaddb3d58a797d58398ece5b47746c59a10
Jason Permann <JaceFoxman@gmail.com>
2025-04-23 08:22:47 -0600

    Remove unecessary lines

--- Etch-A-Sketch/Etch-A-Sketch.vb
+++ Etch-A-Sketch/Etch-A-Sketch.vb
@@ -10,7 +10,6 @@ Option Compare Text
 
 Imports System.Threading.Thread
 Public Class DrawingForm
-
     'Set Color__________________________________________________________________________
     Function SetColor(Optional newColor As Color = Nothing) As Color
         Static _forecolor As Color = Color.Black
@@ -26,7 +25,6 @@ Public Class DrawingForm
         SetColor(ColorDialog.Color)
         Return SetColor()
     End Function
-
     'Draw with mouse_____________________________________________________________________________________________
     Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseMove
         Static oldx, oldy As Integer
@@ -64,7 +62,6 @@ Public Class DrawingForm
             x += (DrawingPictureBox.Width \ 10)
             graphics.DrawLine(pen, x, 0, x, DrawingPictureBox.Height)
         Loop
-
     End Sub
     Sub SineWave()
         Dim graphics As Graphics = DrawingPictureBox.CreateGraphics
@@ -80,9 +77,7 @@ Public Class DrawingForm
             oldx = x
             oldy = newy
         Next
-
     End Sub
-
     Sub CosineWave()
         Dim graphics As Graphics = DrawingPictureBox.CreateGraphics
         Dim pen As New Pen(Color.Blue)
@@ -97,9 +92,7 @@ Public Class DrawingForm
             oldx = x
             oldy = newy
         Next
-
     End Sub
-
     Sub TangentWave()
         Dim graphics As Graphics = DrawingPictureBox.CreateGraphics
         Dim pen As New Pen(Color.Red)
@@ -118,9 +111,7 @@ Public Class DrawingForm
         ElseIf DrawingPictureBox.Width > 2400 Then
 
         End If
-
     End Sub
-
     'Clear Functions___________________________________________________________________________________________________________________________________________________________________
     Sub ShakeAndClear()
         Dim movePosition As Integer = RNG(1, 350) 'RNG not nedded just added for randomness on the shake
@@ -136,7 +127,6 @@ Public Class DrawingForm
             Sleep(100)
             movePosition *= -1 'need this to bring back to original position
         Next
-
         DrawingPictureBox.Refresh()
     End Sub
 
@@ -152,11 +142,9 @@ Public Class DrawingForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click, ExitToolStripMenuItem.Click, ExitToolStripMenuItem1.Click
         Me.Close()
     End Sub
-
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click, ClearToolStripMenuItem.Click, ClearToolStripMenuItem1.Click
         ShakeAndClear()
     End Sub
-
     Private Sub DrawWaveformButton_Click(sender As Object, e As EventArgs) Handles DrawWaveformButton.Click, DrawWaveformsToolStripMenuItem.Click, DrawWaveformToolStripMenuItem.Click
         ShakeAndClear()
         Graticules()
@@ -164,11 +152,9 @@ Public Class DrawingForm
         CosineWave()
         TangentWave()
     End Sub
-
     Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click, SelectColorToolStripMenuItem.Click, SetColorToolStripMenuItem.Click
         DialogBox()
     End Sub
-
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
         Me.Hide()
         AboutForm.Show()
